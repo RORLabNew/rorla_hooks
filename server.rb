e7324bbbfe998bb2217038bfdbed4035289763ac
seapy <iamseapy@gmail.com>
2014-08-20 16:59:16 +0900

    초기 커밋

---
+++ server.rb
@@ -0,0 +1,39 @@
+require 'rubygems'
+require 'bundler'
+
+Bundler.require
+
+require 'sinatra/base'
+require 'webrick'
+require 'webrick/https'
+require 'openssl'
+
+CERT_PATH = '.'
+
+webrick_options = {
+  :Port               => 8443,
+  :Logger             => WEBrick::Log::new($stderr, WEBrick::Log::INFO),
+  :DocumentRoot       => '.',
+  :SSLEnable          => true,
+  :SSLVerifyClient    => OpenSSL::SSL::VERIFY_NONE,
+  :SSLCertificate     => OpenSSL::X509::Certificate.new(File.open(File.join(CERT_PATH, "server.crt")).read),
+  :SSLPrivateKey      => OpenSSL::PKey::RSA.new(File.open(File.join(CERT_PATH, "server.key")).read),
+  :SSLCertName        => [ [ "CN",WEBrick::Utils::getservername ] ]
+}
+
+class MyServer < Sinatra::Base
+  get '/' do
+    "rorla deploy hook!\n"
+  end
+
+  post '/reload' do
+    result = system('/root/script/rorla_reload.sh')
+    "result => #{result}\n"
+  end
+end
+
+server = ::Rack::Handler::WEBrick
+trap(:INT) do
+  server.shutdown
+end
+server.run(MyServer, webrick_options)
